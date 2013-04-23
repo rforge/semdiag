bf27e21f28b55ef74c2930db8b3a13dd3e6a039b
johnnyzhz <johnnyzhz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-23 20:14:44 +0000

    New combined function for rate and view
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/semdiag@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rstats/R/rstats.R
+++ pkg/rstats/R/rstats.R
@@ -1,105 +1,26 @@
-## R function to rate the package
-download<-function(package="base",lib.loc = NULL){
+rate<-function(package="base", download=NULL, like=NULL, rating=NULL, comment=NULL, lib.loc = NULL){
 	dir <- system.file(package = package, lib.loc = lib.loc)
     if (dir == "") {
 		gettextf("You have not installed the package %s. You can only rate a package you installed already. Thanks. ", sQuote(package))
 	}else{
 		meta <- packageDescription(pkg = package)
-		meta <- URLencode(paste(meta$Version, ";", meta$Built, ";", meta$Maintainer, ";", meta$Repository), TRUE)
+		meta <- paste(meta$Version, ";", meta$Built, ";", meta$Maintainer, ";", meta$Repository)
+		if (!is.null(download)) download <- '1'
+		if (!is.null(like)) like <- '1'
+		if (!is.null(rating)){
+			if (!(rating %in% 1:5)) stop('The rating has to be 1 from 5.')
+			rating <- as.character(rating)
+		}			
 		dir <- system.file(package = "RCurl", lib.loc = lib.loc)
-		if (dir == ""){
-			URL<-paste('http://rstats.psychstat.org/rate.php?type=1&name=',  package, '&meta=', meta, sep='')
+		if (dir == ""){	
+			URL<-paste('http://rstats.psychstat.org/rateGet.php?name=', package, '&download=', download, '&like=', like, '&rating=', rating, '&comment=',URLencode(comment,TRUE), '&meta=', URLencode(meta,TRUE), sep='')
 			browseURL(URL)
 		}else{
 			library('RCurl')
-			getForm("http://rstats.psychstat.org/rate.php", type='1', name=package, meta=meta, rcurl='1')
+			postForm("http://rstats.psychstat.org/rate.php", name=package, download=download, like=like, rating=rating, comment=comment, meta=meta)
+			cat('Thanks for your feedback!\n')
 		}
 	}
-	cat("Thanks for your feedback!\n")
-}
-
-like<-function(package="base",lib.loc = NULL){
-	dir <- system.file(package = package, lib.loc = lib.loc)
-    if (dir == "") {
-		gettextf("You have not installed the package %s. You can only rate a package you installed already. Thanks. ", sQuote(package))
-	}else{
-		meta <- packageDescription(pkg = package)
-		meta <- URLencode(paste(meta$Version, ";", meta$Built, ";", meta$Maintainer, ";", meta$Repository), TRUE)
-		dir <- system.file(package = "RCurl", lib.loc = lib.loc)
-		if (dir == ""){
-			URL<-paste('http://rstats.psychstat.org/rate.php?type=2&name=', package, '&meta=', meta, sep='')
-			browseURL(URL)
-		}else{
-			library('RCurl')
-			getForm("http://rstats.psychstat.org/rate.php", type='2', name=package, meta=meta, rcurl='1')
-		}
-	}		
-	cat("Thanks for your feedback!\n")
-}
-
-dislike<-function(package="base",lib.loc = NULL){
-	dir <- system.file(package = package, lib.loc = lib.loc)
-    if (dir == "") {
-		gettextf("You have not installed the package %s. You can only rate a package you installed already. Thanks. ", sQuote(package))
-	}else{
-		meta <- packageDescription(pkg = package)
-		meta <- URLencode(paste(meta$Version, ";", meta$Built, ";", meta$Maintainer, ";", meta$Repository), TRUE)
-		dir <- system.file(package = "RCurl", lib.loc = lib.loc)
-		if (dir == ""){
-			URL<-paste('http://rstats.psychstat.org/rate.php?type=3&name=', package, '&meta=', meta, sep='')
-			browseURL(URL)
-		}else{
-			library('RCurl')
-			getForm("http://rstats.psychstat.org/rate.php", type='3', name=package, meta=meta, rcurl='1')
-		}
-	}
-	cat("Thanks for your feedback!\n")	
-}
-
-rate<-function(package="base",rating=5,lib.loc = NULL){
-	dir <- system.file(package = package, lib.loc = lib.loc)
-    if (dir == "") {
-		gettextf("You have not installed the package %s. You can only rate a package you installed already. Thanks. ", sQuote(package))
-	}else{
-		if (rating %in% 1:5){	
-			meta <- packageDescription(pkg = package)
-			meta <- URLencode(paste(meta$Version, ";", meta$Built, ";", meta$Maintainer, ";", meta$Repository), TRUE)
-			dir <- system.file(package = "RCurl", lib.loc = lib.loc)
-			if (dir == ""){
-				URL<-paste( 'http://rstats.psychstat.org/rate.php?type=4&name=', package, '&rating=', rating, '&meta=', meta, sep='')
-				browseURL(URL)
-			}else{
-				library('RCurl')
-				getForm("http://rstats.psychstat.org/rate.php", type='4', name=package, rating=rating, meta=meta, rcurl='1')
-			}
-				cat("Thanks for your feedback!\n")
-		}else{
-			cat('The rating has to be 1 from 5.')
-		}
-	}
-}
-
-Comment<-function(package="base",comment=NULL,lib.loc = NULL){ 
-	dir <- system.file(package = package, lib.loc = lib.loc)
-    if (dir == "") {
-		gettextf("You have not installed the package %s. You can only rate a package you installed already. Thanks. ", sQuote(package))
-	}else{
-		if (!is.null(comment)){
-			meta <- packageDescription(pkg = package)
-			meta <- URLencode(paste(meta$Version, ";", meta$Built, ";", meta$Maintainer, ";", meta$Repository), TRUE)
-			dir <- system.file(package = "RCurl", lib.loc = lib.loc)
-			if (dir == ""){	
-				URL<-paste('http://rstats.psychstat.org/rate.php?type=5&name=', package, '&comment=',URLencode(comment, TRUE), '&meta=', meta, sep='')
-				browseURL(URL)
-			}else{
-				library('RCurl')
-				getForm("http://rstats.psychstat.org/rate.php", type='5', name=package, comment=URLencode(comment, TRUE), meta=meta, rcurl='1')
-				cat('Thanks for your feedback!\n')
-			}
-		}else{
-		  cat('Please provide your comment first.')
-		}
-	}		
 }
 
 view<-function(package="base",comment=FALSE, ncomment=1:5, lib.loc = NULL){
@@ -109,30 +30,22 @@ view<-function(package="base",comment=FALSE, ncomment=1:5, lib.loc = NULL){
 		browseURL(URL)
 	}else{
 		library('RCurl')
-		rating<-getURL(paste('http://rstats.psychstat.org/comments.php?name=',  package, sep=''))
-		temp<-strsplit(rating, "\n")
-		rate<-temp[[1]][2]
-		rate<-unlist(strsplit(rate, ";", fixed=TRUE))
-		for (i in 1:4){
-			cat(rate[i], "\n")
-		}
-		cat("\n")
-		if (comment){
-			if (length(temp[[1]])==3) stop("No comment available yet")
-			a<-temp[[1]][4]
-			b<-strsplit(a, "</tr><tr>", fixed=TRUE)
-			d<-strsplit(b[[1]], "</td> <td>", fixed=TRUE)
-			nd<-length(d)-1
-			cat("There are ", nd, " comments in total.\n")
-			if (max(ncomment)>nd){
-				ncomment<-1:nd
+		rating<-getURL(paste('http://rstats.psychstat.org/view.php?name=',  package, sep=''))
+		rate<-strsplit(rating, "\n")[[1]]
+		nrate<-length(rate)
+		if (nrate>2){
+			for (i in 1:3) cat(rate[i], "\n")
+			cat("\n")		
+			if (comment){
+				if (nrate==3) stop("No comment available yet")
+				nd<-nrate-3
+				cat("There are ", nd, " comments in total.\n")
+				if (max(ncomment)>nd) ncomment<-1:nd
+				for (i in (ncomment+3)) cat(rate[i], "\n")
 			}
-			for (i in (ncomment+1)){
-				txt<-d[[i]]
-				txt<-sub("</td>","",txt[2],fixed=TRUE)
-				txt<-sub("</tr>","",txt,fixed=TRUE)
-				cat(i-1, txt, "\n")
-			}
+		}else{
+			cat(rating)	
 		}
 	}		
 }
+
