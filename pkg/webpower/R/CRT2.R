304cf7fbd87ffe41de16e67aeacbac0e31a74514
johnnyzhz <johnnyzhz@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-07-20 13:14:33 +0000

    WebPower
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/semdiag@130 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/webpower/R/CRT2.R
@@ -0,0 +1,26 @@
+#' @docType data
+#' @title Example Data For CRT With 2 Arms
+#' @keywords datasets
+#' @name CRT2
+#' @usage CRT2
+#' @description
+#' \itemize{
+#'  \item ID. The identification number of the subjects.
+#'  \item cluster. The cluster number.
+#'  \item score. The score of the subject.
+#'  \item group. The group number.
+#' }
+#' @examples
+#' CRT2
+#' #  ID cluster score group
+#' #  1 1 6 0
+#' #  2 1 2 0
+#' #  3 2 6 1
+#' #  4 2 5 1
+#' #  5 3 1 0
+#' #  6 3 4 0
+#' #  7 4 6 1
+#' #  8 4 4 1
+'CRT2'
+
+
